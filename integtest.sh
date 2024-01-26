445c423177f656875e4f1a82468926fa63c1a066
Peter Zhu <zhujiaxi@amazon.com>
2024-01-26 14:23:15 -0500

    Add more
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- integtest.sh
+++ integtest.sh
@@ -90,8 +90,8 @@ PASSWORD=`echo $CREDENTIAL | awk -F ':' '{print $2}'`
 # User can send custom browser path through env variable
 if [ -z "$BROWSER_PATH" ]
 then
-  BROWSER_PATH="chromium"
-  #BROWSER_PATH="electron"
+  #BROWSER_PATH="chromium"
+  BROWSER_PATH="firefox"
 fi
 
 . ./test_finder.sh
